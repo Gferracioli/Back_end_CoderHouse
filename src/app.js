a72c6f0c8577ec09c1163257b3052d1eef3f05c9
Gferracioli <gustavo.freelancer.site@gmail.com>
2024-11-21 15:16:42 -0300

    tentando os sockets

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
 const productsRouter = require('./routes/products');
 const cartsRouter = require('./routes/carts');
 
 const app = express();
+
+// Configuração do Handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 // Middleware para interpretar o corpo das requisições em URL-encoded e JSON
 app.use(express.urlencoded({ extended: true }));
--- src/server.js
+++ src/server.js
@@ -1,7 +1,28 @@
 const app = require('./app');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const PORT = 8080;
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Configurando o Socket.IO
+io.on('connection', (socket) => {
+  console.log('Novo cliente conectado');
+
+  // Listener de exemplo para uma mensagem enviada pelo cliente
+  socket.on('mensagem_cliente', (mensagem) => {
+    console.log('Mensagem recebida do cliente:', mensagem);
+    // Enviar uma resposta de volta ao cliente
+    socket.emit('mensagem_servidor', 'Mensagem recebida no servidor');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
+  });
+});
+
+// Subindo o servidor
+server.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
